fe53d07494f573c1544d8a43350c3c6498ffd689
tingamapuro04 <alponcemobutu100@gmail.com>
2022-12-02 23:51:26 +0300

    linter errors

--- app.js
+++ app.js
@@ -1,40 +1,31 @@
-const img_1 = document.querySelector('#img_1')
-const img_2 = document.querySelector('#img_2')
-const img_3 = document.querySelector('#img_3')
-const rotation = document.querySelector('#rotations');
+const img1 = document.querySelector('#img_1');
+const img2 = document.querySelector('#img_2');
+const img3 = document.querySelector('#img_3');
 
 const aliceTumbling = [
-  { transform: "rotate(0) scale(1)" },
-  { transform: "rotate(360deg) scale(0)" },
+  { transform: 'rotate(0) scale(1)' },
+  { transform: 'rotate(360deg) scale(0)' },
 ];
 
 const aliceTiming = {
   duration: 3000,
   iterations: 2,
-  direction: "alternate-reverse",
-  fill: "backwards",
+  direction: 'alternate-reverse',
+  fill: 'backwards',
 };
 
-const first = () => {
-  return img_1.animate(aliceTumbling, aliceTiming).finished
-} 
-const second = () => {
-  return img_2.animate(aliceTumbling, aliceTiming).finished
-} 
-const third = () => {
-  return img_3.animate(aliceTumbling, aliceTiming).finished
-} 
+const first = () => img1.animate(aliceTumbling, aliceTiming).finished;
+const second = () => img2.animate(aliceTumbling, aliceTiming).finished;
+const third = () => img3.animate(aliceTumbling, aliceTiming).finished;
 
 const handleanimation = async () => {
   try {
-    await first()
-    await second()
-    await third()
+    await first();
+    await second();
+    await third();
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 
-handleanimation()
-
-
+handleanimation();
