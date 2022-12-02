b6f6721a6587c085f0de345eb5153cd20668ad73
tingamapuro04 <alponcemobutu100@gmail.com>
2022-12-02 20:33:21 +0300

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Mobutu")
+const temo = new Promise(function(resolve, reject) {
+  const time1 = 20;
+  const time2 = 21;
+
+  if (time1 === 25) {
+    resolve()
+  } else {
+    reject()
+  }
+});
+
+temo
+  .then(() => console.log('Our promise is fulfilled'))
+  .catch(() => console.log('Our promise has failed!'))
